3ee388862c83ff1071b5b9b5eee10b45f3bf69d2
Patrick Raedler <code@readpato.dev>
2023-12-01 23:18:40 +0100

    refactor(community): add copilot with own keybindings

--- plugins/community.lua
+++ plugins/community.lua
@@ -29,5 +29,21 @@ return {
   -- Motions
   { import = "astrocommunity.motion.flit-nvim" },
   -- Completion
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  {
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+          --dismiss = false,
+        },
+      },
+    },
+  },
 }
