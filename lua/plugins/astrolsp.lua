eff25e3185fe7ded101835b66334bb922c18b909
Patrick Raedler <code@readpato.dev>
2024-05-01 18:12:48 +0200

    refactor(astrolsp): add files for volar to recognize

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -69,6 +69,9 @@ return {
           "markdown",
         },
       },
+      volar = {
+        filetypes = { "vue", "typescript", "javascript" },
+      },
     },
     -- customize how language servers are attached
     handlers = {
