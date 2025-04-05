ff63a7b4401d54cfb850c0bf8e37431354218cfa
Patrick Raedler <code@readpato.dev>
2025-04-05 23:52:08 +0200

    chore(community): add go package

--- lua/community.lua
+++ lua/community.lua
@@ -6,10 +6,10 @@ return {
   { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.recipes.ai" },
   { import = "astrocommunity.completion.blink-cmp" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.flit-nvim" },
-  -- import/override with your plugins folder
 }
